45b2d010581abb8b707b50c9ef544533d1d662ce
Demayne Collins <Collinsd73@gmail.com>
2020-06-29 22:45:55 -0400

    update cylinder test file to include function1 and 2

--- tests_volumes/test_cylinder.py
+++ tests_volumes/test_cylinder.py
@@ -6,16 +6,17 @@ import math
 os.system('cls')
 
 
-@pytest.mark.function
+@pytest.mark.function1
 def tests_cylinder():
     cylinder = volumes.Cylinder(5, 10, "cubic meters")
-    assert cylinder.height == 10
-    assert cylinder.radius == 5
     assert cylinder._unit_of_measurement == "cubic meters"
 
 
-@pytest.mark.function
+@pytest.mark.function2
 def tests_vol_cylinder():
+    cylinder = volumes.Cylinder(5, 10, "cubic meters")
+    assert cylinder.height == 10
+    assert cylinder.radius == 5
     result = math.pi * (5 ** 2) * 10 == 785
     assert result != str
 
